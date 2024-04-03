a34004f66c10613fd91a7a57cf60ef7270113d91
Vitor Cerqueira <vcerq@vitors-mbp.lan>
2024-04-03 14:58:26 +0100

    auto lag

--- scripts/analysis/auto_lags.py
+++ scripts/analysis/auto_lags.py
@@ -1,17 +1,42 @@
 import pandas as pd
 from neuralforecast.losses.numpy import smape, rmae
 
-results = pd.read_csv('assets/results/Tourism_Monthly_lags.csv')
+from codebase.workflows.config import LAGS
 
-cols_bool = results.columns.str.contains('NHITS')
-col_names = results.columns[cols_bool]
+ds = 'M3'
+results = pd.read_csv(f'assets/results/{ds}_Monthly_lags.csv')
 
-results_g = results.groupby('unique_id')
+lags = LAGS[ds]
+
+predictions = {m: results[f'NHITS_{int(lag)}'] for m, lag in lags.items()}
+predictions = pd.DataFrame(predictions)
+
+methods = predictions.columns.tolist() + ['SeasonalNaive']
+
+results_all = pd.concat([results, predictions], axis=1)
+
+cols_bool = results_all.columns.str.contains('NHITS')
+results_all = results_all.loc[:, ~cols_bool]
+
+# overall error
+
+overall_sc = {}
+for k in methods:
+    overall_sc[k] = smape(y=results_all['y'], y_hat=results_all[k])
+    # sc[k] = rmae(y=df['y'], y_hat1=df[k], y_hat2=df['SeasonalNaive'])
+
+overall_sc = pd.Series(overall_sc)
+overall_sc.sort_values()
+
+
+# by series
+
+results_g = results_all.groupby('unique_id')
 
 scores = []
 for g, df in results_g:
     sc = {}
-    for k in col_names:
+    for k in methods:
         sc[k] = smape(y=df['y'], y_hat=df[k])
         # sc[k] = rmae(y=df['y'], y_hat1=df[k], y_hat2=df['SeasonalNaive'])
 
@@ -20,5 +45,10 @@ for g, df in results_g:
 scores_df = pd.DataFrame(scores)
 scores_df.index = [*results_g.groups]
 
-scores_df.mean()
-scores_df.apply(lambda x: x[x > x.quantile(0.9)].mean())
+scores_df.rank(axis=1).mean().sort_values()
+
+scores_df.boxplot()
+scores_df.mean().sort_values()
+scores_df.median().sort_values()
+scores_df.apply(lambda x: x[x > x.quantile(0.9)].mean()).sort_values()
+
